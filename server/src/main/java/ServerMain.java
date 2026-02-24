899c60a38947bce203ffdb722bdfd38a5d2acdd5
Packer Barlow <packerb2@byu.edu>
2026-02-24 16:43:03 -0700

    got the beggining server page running

--- server/src/main/java/ServerMain.java
+++ server/src/main/java/ServerMain.java
@@ -1,7 +1,11 @@
 import chess.*;
+import server.Server;
 
 public class ServerMain {
     public static void main(String[] args) {
+        Server server = new Server();
+        server.run(8080);
+
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
     }
