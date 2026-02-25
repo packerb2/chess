3ffd5247841891b6819487905d5dac38e44614dc
Packer Barlow <packerb2@byu.edu>
2026-02-24 17:40:35 -0700

    built skeleton for endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,18 +1,83 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
 
-import java.util.UUID;
+import io.javalin.http.Context;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 
 public class Server {
 
     private final Javalin javalin;
 
+    final private HashSet<String> validTokens = new HashSet<>(Set.of("secret1", "secret2"));
+
+    private boolean authorized(Context ctx) {
+        String authToken = ctx.header("authorization");
+        if (!validTokens.contains(authToken)) {
+            ctx.contentType("application/json");
+            ctx.status(401);
+            ctx.result(new Gson().toJson(Map.of("msg", "invalid authorization")));
+            return false;
+        }
+        return true;
+    }
+
+    private void clear(Context context) {
+        return;
+    }
+
+    private void register(Context context) {
+        return;
+    }
+
+    private void login(Context context) {
+        return;
+    }
+
+    private void logout(Context context) {
+        if (authorized(context)) {
+            return;
+        }
+        return;
+    }
+
+    private void listGames(Context context) {
+        if (authorized(context)) {
+            return;
+        }
+        return;
+    }
+
+    private void createGame(Context context) {
+        if (authorized(context)) {
+            return;
+        }
+        return;
+    }
+
+    private void joinGame(Context context) {
+        if (authorized(context)) {
+            return;
+        }
+        return;
+    }
+
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        javalin = Javalin.create(config -> config.staticFiles.add("web"))
 
-        // Register your endpoints and exception handlers here.
-
+                // Register your endpoints and exception handlers here.
+                .post("/name{name}", this::register)
+                .post("/name{name}", this::login)
+                .delete("/name{name}", this::logout)
+                .get("/name{name}", this::listGames)
+                .post("/name{name}", this::createGame)
+                .put("/name{name}", this::joinGame)
+                .delete("/name{name}", this::clear);
     }
 
     public int run(int desiredPort) {
