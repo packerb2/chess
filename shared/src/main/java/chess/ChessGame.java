47d731a9e3c761c0bdb0462643eae8e4e242d5fa
Packer Barlow <packerb2@byu.edu>
2026-03-03 11:42:21 -0700

    removed some unneeded comments

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -196,23 +196,6 @@ public class ChessGame {
     public boolean isInCheckmate(TeamColor teamColor) {
         if (isInCheck(teamColor)) {
             return isSurrounded(teamColor);
-//            ChessPosition spot;
-//            ChessPiece piece;
-//            int x = 0;
-//            while (x++ < 8) {
-//                int y = 0;
-//                while (y++ < 8) {
-//                    spot = new ChessPosition(x, y);
-//                    piece = currentBoard.getPiece(spot);
-//                    if (piece != null && piece.getTeamColor() == teamColor) {
-//                        Collection<ChessMove> escape = validMoves(spot);
-//                        if (!escape.isEmpty()) {
-//                            return false;
-//                        }
-//                    }
-//                }
-//            }
-//            return true;
         }
         return false;
     }
@@ -227,23 +210,6 @@ public class ChessGame {
     public boolean isInStalemate(TeamColor teamColor) {
         if (!isInCheck(teamColor)) {
             return isSurrounded(teamColor);
-//            ChessPosition spot;
-//            ChessPiece piece;
-//            int x = 0;
-//            while (x++ < 8) {
-//                int y = 0;
-//                while (y++ < 8) {
-//                    spot = new ChessPosition(x, y);
-//                    piece = currentBoard.getPiece(spot);
-//                    if (piece != null && piece.getTeamColor() == teamColor) {
-//                        Collection<ChessMove> escape = validMoves(spot);
-//                        if (!escape.isEmpty()) {
-//                            return false;
-//                        }
-//                    }
-//                }
-//            }
-//            return true;
         }
         return false;
     }
