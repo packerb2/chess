42300a42c3884b5213aaf00e1bab3a95effd0022
Packer Barlow <packerb2@byu.edu>
2026-03-12 10:58:12 -0600

    got my database to be passing prev tests with SQLUsers

--- server/src/main/java/dataaccess/SQLDataAccess.java
+++ server/src/main/java/dataaccess/SQLDataAccess.java
@@ -57,9 +57,27 @@ public class SQLDataAccess implements DataAccess{
         }
     }
 
+    private final String[] createStatements = {
+        """
+        CREATE TABLE IF NOT EXISTS users (
+            `username` varchar(255) NOT NULL,
+            `password` varchar(255) NOT NULL,
+            `email` varchar(255) NOT NULL,
+             PRIMARY KEY (`username`)
+        )
+        """
+    };
 
     private void configureDatabase() throws DataAccessException {
         DatabaseManager.createDatabase();
-        Connection conn = DatabaseManager.getConnection();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
     }
 }
